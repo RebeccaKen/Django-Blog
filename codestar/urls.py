34b183850b712a5c30e6d9f4e291f858646bf2c7
RebeccaKen <kennedyrebecca694@gmail.com>
2023-03-09 21:57:21 +0000

    Add allauth installation

--- codestar/urls.py
+++ codestar/urls.py
@@ -1,5 +1,4 @@
 """codestar URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
@@ -20,4 +19,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("blog.urls"), name="blog-urls"),
     path('summernote/', include('django_summernote.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
